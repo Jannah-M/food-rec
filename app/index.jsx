f9e5e2bf374c8801a2124b3db1f4a9985bcfa969
vianchase <vctibule@gmail.com>
2026-05-19 16:35:41 -0700

    Set up root component and export screens

--- app/index.jsx
+++ app/index.jsx
@@ -1,1 +1,9 @@
-# placeholder
+import { registerRootComponent } from 'expo';
+
+import App from './App';
+
+registerRootComponent(App);
+
+export { default as OnboardingScreen } from './OnboardingScreen';
+export { default as RecommendationScreen } from './RecommendationScreen';
+export { default as AcceptedRestaurantScreen } from './AcceptedRestaurantScreen';
